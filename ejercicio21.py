e3cb266a9fa371eed3f0145e1a11fac73ffe36bb
Yanina Codega <106331416+ycode5@users.noreply.github.com>
2022-11-09 07:38:30 -0300

    Update ejercicio21.py

--- ejercicio21.py
+++ ejercicio21.py
@@ -1,27 +1,10 @@
-import tkinter
-from  tkinter import ttk
-window=tkinter.Tk()
-
-window.columnconfigure(0, weight=1)
-window.columnconfigure(1, weight=3)
-
-boton_seleccionado= tkinter.StringVar()
-boton_seleccionado1= tkinter.StringVar()
-boton_seleccionado2= tkinter.StringVar()
-boton_seleccionado3= tkinter.StringVar()
-
-checkbox=ttk.Checkbutton(window,text='opcion 1',variable=boton_seleccionado)
-checkbox.grid(column=0,row=1)
-checkbox=ttk.Checkbutton(window,text='opcion 2',variable=boton_seleccionado1)
-checkbox.grid(column=0,row=2)
-checkbox=ttk.Checkbutton(window,text='opcion 3',variable=boton_seleccionado2)
-checkbox.grid(column=0,row=3)
-checkbox=ttk.Checkbutton(window,text='opcion 4',variable=boton_seleccionado3)
-checkbox.grid(column=0,row=4)
-
-
-lista_label=ttk.Label(window, text='Lista: ')
-lista_label.grid(column=0,row=0,sticky=tkinter.W, padx=5,pady=5)
-
-
-window.mainloop()
+from tkinter import *
+master = Tk()
+nombres = StringVar()
+listbox = Listbox(master)
+for item in ["Jose", "Claudia", "Alejandro", "Florencia", "Miguel", "Melisa", "Simon", "Antonio"]:
+ listbox.insert(END, item)
+listbox.pack()
+label = Label(text="Lista de nombres de personas")
+label.pack()
+master.mainloop()
